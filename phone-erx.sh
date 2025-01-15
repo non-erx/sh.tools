925c3394ca488339c352467d6d682b58282a1f97
non-erx <serhiisymonchukca@gmail.com>
2025-01-14 20:23:48 -0500

    autodelete of tabby

--- phone-erx.sh
+++ phone-erx.sh
@@ -155,7 +155,7 @@ setup_system_apps() {
         apt update && apt install -y android-studio
         
         # Tabby
-        rm -rf tabby-1.0.219-linux-arm64.deb
+        rm -rf tabby-1.0.219-linux-arm64*
         wget "https://github.com/Eugeny/tabby/releases/download/v1.0.219/tabby-1.0.219-linux-arm64.deb"
         sudo dpkg -i tabby-1.0.219-linux-arm64.deb | sudo apt instal -f -y
         rm tabby-1.0.219-linux-arm64.deb
