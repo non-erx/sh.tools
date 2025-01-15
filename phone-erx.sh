77b82d76b2fddcd0a13bb03e19d09da49fb0e23a
non-erx <serhiisymonchukca@gmail.com>
2025-01-14 20:22:07 -0500

    bug fix

--- phone-erx.sh
+++ phone-erx.sh
@@ -155,8 +155,9 @@ setup_system_apps() {
         apt update && apt install -y android-studio
         
         # Tabby
+        rm -rf tabby-1.0.219-linux-arm64.deb
         wget "https://github.com/Eugeny/tabby/releases/download/v1.0.219/tabby-1.0.219-linux-arm64.deb"
-        sudo dpkg -i tabby-1.0.219-linux-arm64.deb | apt instal -f -y
+        sudo dpkg -i tabby-1.0.219-linux-arm64.deb | sudo apt instal -f -y
         rm tabby-1.0.219-linux-arm64.deb
         
         # Zen Browser
